6e6ca7316594e1d433f4c6d155dc7aaa241e9328
Pavel Burgr <pb@drake.cz>
2022-07-05 16:58:05 +0200

    WIP

---
+++ __init__.py
@@ -0,0 +1,39 @@
+from machine import Timer
+from machine import Pin
+import network
+
+
+def init(ssid, password, ledPin=2, ledLogic=False):
+
+    pin = Pin(ledPin, Pin.OUT)
+
+    pin.off()
+
+    ledState = False
+    connectionState = False
+
+    def tick():
+        connected = wlan.isconnected()
+
+        nonlocal ledState
+        ledState = (not connected and not ledState)
+        pin.value(ledState == ledLogic)
+
+        nonlocal connectionState
+        if connected != connectionState:
+            connectionState = connected
+            if connected:
+                print("WiFi connected, IP:", wlan.ifconfig()[0])
+            else:
+                print("WiFi disconnected") 
+
+
+    tim = Timer(-1)
+    tim.init(period=100, mode=Timer.PERIODIC, callback=lambda t: tick())
+
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+
+    print("Connecting to WiFi...")
+    wlan.connect(ssid, password)
+
