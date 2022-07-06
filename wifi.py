93ffe05809e479cab7c5741d37d0539b6fa06f79
Pavel Burgr <pb@drake.cz>
2022-07-06 12:17:42 +0200

    WIP

--- wifi.py
+++ wifi.py
@@ -3,37 +3,32 @@ from machine import Pin
 import network
 
 
-def init(ssid, password, ledPin=2, ledLogic=True):
+def init(ssid, password, ledPin=2, ledLogic=False):
 
     pin = Pin(ledPin, Pin.OUT)
 
-    pin.off()
-
-    ledState = False
     connectionState = False
 
-    def tick():
+    def check():
         connected = wlan.isconnected()
-
-        nonlocal ledState
-        ledState = (not connected and not ledState)
-        pin.value(ledState == ledLogic)
 
         nonlocal connectionState
         if connected != connectionState:
             connectionState = connected
             if connected:
                 print("WiFi connected, IP:", wlan.ifconfig()[0])
-            else:
-                print("WiFi disconnected") 
+                pin.value(ledLogic)
 
-
-    tim = Timer(-1)
-    tim.init(period=100, mode=Timer.PERIODIC, callback=lambda t: tick())
+        if not connected:
+            pin.value(not pin.value())
+            if wlan.status() != network.STAT_CONNECTING:
+                print("Connecting to WiFi...")
+                wlan.connect(ssid, password)
 
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
 
-    print("Connecting to WiFi...")
-    wlan.connect(ssid, password)
+    tim = Timer(-1)
+    tim.init(period=100, mode=Timer.PERIODIC, callback=lambda t: check())
+
 
