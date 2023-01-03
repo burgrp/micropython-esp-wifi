9912dfed73fe370073714c5a6f69079580c72b87
Pavel Burgr <pb@drake.cz>
2023-01-03 18:13:31 +0100

    WIP

--- esp_wifi.py
+++ esp_wifi.py
@@ -4,31 +4,33 @@ import virtual_timer
 from machine import Timer
 
 
-def init(ssid, password, ledPin=2, ledLogic=False):
+def init(ssid=None, password=None, ledPin=2, ledLogic=False):
 
     pin = Pin(ledPin, Pin.OUT)
+    pin.value(ledLogic)
 
     connectionState = False
 
-    def check():
+    def check(timer):
         connected = wlan.isconnected()
 
         nonlocal connectionState
         if connected != connectionState:
             connectionState = connected
             if connected:
-                print("WiFi connected, IP:", wlan.ifconfig()[0])
+                print('WiFi connected, IP:', wlan.ifconfig()[0])
                 pin.value(ledLogic)
+            if not connected:
+                print('WiFi disconnected')
 
         if not connected:
             pin.value(not pin.value())
             if wlan.status() != network.STAT_CONNECTING:
-                print("Connecting to WiFi SSID", ssid)
+                print('Connecting to WiFi SSID:', ssid)
                 wlan.connect(ssid, password)
 
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
 
-    virtual_timer.new().init(period=100, mode=Timer.PERIODIC, callback=lambda t: check())
-
+    virtual_timer.VirtualTimer().init(period=500, mode=Timer.PERIODIC, callback=check)
 
--- setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup
-
-setup(
-    version="1.0.1",
-    py_modules=["wifi"],
-    requires=["git+https://github.com/burgrp/micropython-virtual-timer.git@v1.0.0"]
-)
